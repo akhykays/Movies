c29ef70f00693a2ec64855ea7aebc7de309b965b
akhyhari <akhy.kays@gmail.com>
2026-09-25 22:57:05 +0200

    Domain -> Exception, Entity (base), Movies Rules

---
+++ MoviesCleanArchitecture/MovieManagement/MovieManagement.Domain/Common/DomainException.cs
@@ -0,0 +1,3 @@
+﻿namespace MovieManagement.Domain.Common;
+
+public sealed class DomainException(string message) : Exception(message);
---
+++ MoviesCleanArchitecture/MovieManagement/MovieManagement.Domain/Common/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieManagement.Domain.Common;
+
+public abstract class Entity
+{
+    public Guid Id { get; protected set; } = Guid.CreateVersion7();
+}
+
